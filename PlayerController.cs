6ba0cf24974cfa040d6d57ac3cf8456f883b78b2
Jam Jam James Perey <jamjam.perey@gmail.com>
2023-05-16 23:41:56 -0700

    UPDATED: added flipping of main character snowboarder

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float torqueAmount = 30f;
-    [SerializeField] float jumpAmount = 5f;
+    [SerializeField] float jumpAmount = 20f;
+    Transform playerTransform; 
+    int leftBool = 0;
     // Start is called before the first frame update
     void Start()
     {
        rb2d = GetComponent<Rigidbody2D>();
+       playerTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -21,12 +24,26 @@ public class PlayerController : MonoBehaviour
         } else if(Input.GetKey(KeyCode.RightArrow)){
             rb2d.AddTorque(-torqueAmount);
         }
+        if(Input.GetKey("a") && leftBool == 0){
+            Vector3 currentScale = playerTransform.localScale;
+            currentScale.x *= -1;
+            playerTransform.localScale = currentScale;
+            leftBool = 1;
+            //Debug.Log("player location: " + playerTransform.localScale);
+        }
+        if(Input.GetKey("d") && leftBool == 1){
+            Vector3 currentScale = playerTransform.localScale;
+            currentScale.x *= -1;
+            playerTransform.localScale = currentScale;
+            leftBool = 0;
+        }
         
     }
 
     void OnCollisionStay2D(Collision2D other) {
         if (Input.GetKey(KeyCode.UpArrow)){
-            rb2d.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse);
+                rb2d.AddForce(transform.up * jumpAmount, ForceMode2D.Impulse);
+            
         }
     }
 }
